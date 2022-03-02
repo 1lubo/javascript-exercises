bea64bc0697c2145c183562ee42e21d2559594bf
1Lubo <1Lubo@pm.me>
2022-03-02 13:20:48 +0100

    add solution for exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+let total = 0;
+let beginHere = null;
+let endHere = null;
 
+if ((typeof(start) != 'number') || (typeof(end) != 'number')) {
+    return 'ERROR';
+}
+
+if (start < 0 || end < 0){
+    return 'ERROR';
+}
+
+if (start > end){
+    beginHere = end;
+    endHere = start;
+} else {
+    beginHere = start;
+    endHere = end;
+}
+
+
+
+
+for (beginHere; beginHere < endHere + 1; beginHere++){
+    total += beginHere;
+}
+return total;
 };
 
 // Do not edit below this line
