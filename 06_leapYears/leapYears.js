85e71415de8426560ac062c2e753ce3878506b62
1Lubo <1Lubo@pm.me>
2022-03-02 13:58:40 +0100

    add solution for exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+   if (!isCenturyYear(year) && year % 4 === 0) {
+       return true;
+   } else if (isCenturyYear(year) && year % 400 === 0) {
+       return true;
+   } else {
+       return false;
+   }
+    };
 
-};
+
+const isCenturyYear = year => year % 100 === 0;
 
 // Do not edit below this line
 module.exports = leapYears;
