523c19014b2a8d76d94267b4c320d532805c4c84
1Lubo <1Lubo@pm.me>
2022-03-02 14:06:38 +0100

    add solution for exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  const tempInCel = ((ftemp - 32) * 5) / 9;
+  
+  return Math.round(tempInCel * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function(ctemp) {
+  const tempInFah = ((ctemp * 9) / 5) + 32;
 
+  return Math.round(tempInFah * 10) / 10;
 };
 
 // Do not edit below this line
