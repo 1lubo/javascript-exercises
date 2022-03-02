82a5f531504d450781d8c9734a5e7364a5458af3
1Lubo <1Lubo@pm.me>
2022-03-02 09:45:53 +0100

    add solution for exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, repeatTimes ) {
+    let newString = '';
+    if (repeatTimes < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < repeatTimes; i++) {
+            newString += stringToRepeat;
+        }
+    }
+    return newString;
 };
 
 // Do not edit below this line
