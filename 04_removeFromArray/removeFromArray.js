d9767bcce3d03577b57dc81d278771dcc6043697
1Lubo <1Lubo@pm.me>
2022-03-02 13:08:27 +0100

    add solution for exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const allArgs = Array.from(args);
+    const array = allArgs[0];
+    const toRemove = allArgs.slice(1);
+    const newArray = [];
 
+    for (let i = 0; i < array.length; i++){
+        let curElement = array[i];
+        let indexOfElement = toRemove.indexOf(curElement);
+        if (indexOfElement < 0) {
+            newArray.push(curElement);
+        }
+    }
+    
+    return newArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
