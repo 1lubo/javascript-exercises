5ed99c85b88045c03612e8965ecc50dfff386984
1Lubo <1Lubo@pm.me>
2022-03-09 15:06:23 +0100

    add solution for exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    
+    if (typeof a === "string") {
+        a = parseInt(a);
+    }
+
+    if (a <= 0) {
+        return "OOPS";
+    } 
+    if (a>0 && a<3) {
+        return 1;
+    }
+    let previous = 0;
+    let current = 1;
+    let next = null;
+
+    for (let i = 1; i < a; i++ ) {
+        next = previous + current;
+        previous = current;
+        current = next;
+    }
+
+    return next;
+     
 
 };
 
