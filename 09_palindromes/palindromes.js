f2bec88578a950793912159b784e866d78a7e273
1Lubo <1Lubo@pm.me>
2022-03-09 11:19:53 +0100

    add solution for exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
+
+    let rawLetters = string.toLowerCase().split('');
+    
+    let cleanLetters = rawLetters.filter(function(letter) {
+        return punctuation.indexOf(letter) === -1;
+    })
+    //console.log(cleanLetters.join(''));
+    //console.log(cleanLetters.reverse().join(''));
+    let j = -1;
+    for (let i = 0; i <= Math.floor(cleanLetters.length / 2); i++) {
+        
+        
+        if (cleanLetters[i] != cleanLetters.at(j-i)) {
+            //console.log(`i = ${i} ${cleanLetters[i]} does not equal ${cleanLetters.at(j-i)} j = ${j-i}`);
+            return false; 
+        } 
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
