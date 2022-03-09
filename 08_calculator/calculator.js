4c750f7cdf56e09f80e9fdaf06b402fb2a8ec5a8
1Lubo <1Lubo@pm.me>
2022-03-09 09:52:34 +0100

    add solution for exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
+  
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	/*let numbers = Array.from(arguments);
+  let subNums = numbers.reduce((previousValue, currentValue) => {    
+    previousValue - currentValue}); */
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  let sum = 0;
+  function myFunction(item) {
+    sum += item;
+  }
+  args.forEach(myFunction);
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((previousValue, currentValue) => previousValue * currentValue);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factor = a;
+  if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    for (a; a>=2; a--){
+      factor = factor * (a-1);
+
+    }
+  } return factor;
 };
 
 // Do not edit below this line
