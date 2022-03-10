b273b94e616e85e4c310d4a193acd581362d8a4d
1Lubo <1Lubo@pm.me>
2022-03-10 08:38:11 +0100

    add solution for exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+let date =  new Date().getFullYear();
 
-};
+const findTheOldest = function(pips) {
+    let sorted = pips.sort(function(a,b) {
+      if (!('yearOfDeath' in a)){
+        a['yearOfDeath'] = date;
+        
+      };
+      if (!('yearOfDeath' in b)) {
+        b['yearOfDeath'] = date;
+        
+      }
+      return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1;      
+    });
 
+    return sorted[0];
+  } 
+    
+
+
+
+
+
+  
 // Do not edit below this line
 module.exports = findTheOldest;
